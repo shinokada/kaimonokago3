f46bb677e3c398625ab5c7f6fe93badd0a94a8c4
shinokada <okada.shin@gmail.com>
2014-12-18 12:34:48 +0900

    CI 2.2 update

--- application/config/database.php
+++ application/config/database.php
@@ -39,7 +39,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "luvemile";
+$db['default']['password'] = "sokada";
 $db['default']['database'] = "kaimonokago3";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
